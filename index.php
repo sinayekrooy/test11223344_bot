ef32138b74dd39bd54c99047f4da37d10699f391
sinayekrooy <sinayekrooy@gmail.com>
2016-10-27 09:30:12 +0330

    adding session id

--- index.php
+++ index.php
@@ -1,83 +1,85 @@
 <?php
-session_start();
 set_time_limit(60);
 date_default_timezone_set('Asia/Tehran');
 header('Content-type: application/json');
-    $update = json_decode(file_get_contents('php://input'), true);
+$update = json_decode(file_get_contents('php://input'), true);
 
-    $message = $update["message"];
-    $telegram_id = $message['from']['id'];
-    $userInput = $message['text'];
-    $replyText = "";
+$message = $update["message"];
+$telegram_id = $message['from']['id'];
+session_start();
+session_id($telegram_id);
 
-    if (!isset($_SESSION['appName'])) {
-      switch ($userInput) {
+$userInput = $message['text'];
+$replyText = "";
+
+if (!isset($_SESSION['appName'])) {
+    switch ($userInput) {
         case 'start':
-          $_SESSION['appName'] = "gameStart";
-          break;
+            $_SESSION['appName'] = "gameStart";
+            break;
 
         default:
-          $replyText = "دستور مورد نظر یافت نشد.";
-          $replyText .= "\n";
-          $replyText .= "لطفا یکی از گزینه های موجود را انتخاب کنید:";
-          $replyText .= "\n";
-          $replyText .= "شروع بازی";
-          break;
-      }
+            $replyText = "دستور مورد نظر یافت نشد.";
+            $replyText .= "\n";
+            $replyText .= "لطفا یکی از گزینه های موجود را انتخاب کنید:";
+            $replyText .= "\n";
+            $replyText .= "شروع بازی";
+            break;
     }
+}
 
-    if (isset($_SESSION['appName'])) {
-      if ($_SESSION['appName'] == 'gameStart') {
-          $replyText = gameStart($userInput);
-      }
+if (isset($_SESSION['appName'])) {
+    if ($_SESSION['appName'] == 'gameStart') {
+        $replyText = gameStart($userInput);
     }
+}
 
-    function gameStart($input) {
-        if ($input == 'exit') {
-          session_destroy();
-          return "bye";
-        } elseif ($input == "more") {
+function gameStart($input)
+{
+    if ($input == 'exit') {
+        session_destroy();
+        return "bye";
+    } elseif ($input == "more") {
 
-          $_SESSION['big'] = $_SESSION['rand'];
-          $_SESSION['rand'] = rand($_SESSION['small'], $_SESSION['big']);
+        $_SESSION['big'] = $_SESSION['rand'];
+        $_SESSION['rand'] = rand($_SESSION['small'], $_SESSION['big']);
 
-          $output = "\n" . "آیا عدد انتخاب شده" . $_SESSION['rand'] . "می باشد؟"
-                  . "پاسخ را به صورت '" . "کمتر'" . "'بشتر'" . "'مساوی'" . "ارسال کنید";
-          return $output;
-        }
-        elseif ($input == "less") {
+        $output = "\n" . "آیا عدد انتخاب شده" . $_SESSION['rand'] . "می باشد؟"
+            . "پاسخ را به صورت '" . "کمتر'" . "'بشتر'" . "'مساوی'" . "ارسال کنید";
+        return $output;
+    } elseif ($input == "less") {
 
-         $_SESSION['small'] = $_SESSION['rand'];
-         $_SESSION['rand'] = rand($_SESSION['small'], $_SESSION['big']);
-         $output = "\n" . "آیا عدد انتخاب شده" . $_SESSION['rand'] . "می باشد؟"
-                 . "پاسخ را به صورت '" . "کمتر'" . "'بشتر'" . "'مساوی'" . "ارسال کنید";
-         return $output;
+        $_SESSION['small'] = $_SESSION['rand'];
+        $_SESSION['rand'] = rand($_SESSION['small'], $_SESSION['big']);
+        $output = "\n" . "آیا عدد انتخاب شده" . $_SESSION['rand'] . "می باشد؟"
+            . "پاسخ را به صورت '" . "کمتر'" . "'بشتر'" . "'مساوی'" . "ارسال کنید";
+        return $output;
 
-       } elseif ($input == "equal" ) {
+    } elseif ($input == "equal") {
 
-         $output = "تبریک عدد مورد نظر یافت شد";
-         return $output;
+        $output = "تبریک عدد مورد نظر یافت شد";
+        return $output;
 
-       } elseif ($input == "start") {
+    } elseif ($input == "start") {
 
-          $_SESSION['big'] = 10;
-          $_SESSION['small'] = 1;
-          $_SESSION['rand'] = rand($_SESSION['small'], $_SESSION['big']);
+        $_SESSION['big'] = 10;
+        $_SESSION['small'] = 1;
+        $_SESSION['rand'] = rand($_SESSION['small'], $_SESSION['big']);
 
-          $output = "به بازی حدس عدد خوش آمدید" . "\n" . "یک عدد دلخواه از 1 تا 10 در نظر بگیرید"
+        $output = "به بازی حدس عدد خوش آمدید" . "\n" . "یک عدد دلخواه از 1 تا 10 در نظر بگیرید"
             . "\n" . "آیا عدد انتخاب شده" . $_SESSION['rand'] . "می باشد؟"
             . "پاسخ را به صورت '" . "کمتر'" . "'بشتر'" . "'مساوی'" . "ارسال کنید";
-          return $output;
+        return $output;
 
-        } else {
-          return "دستور وارد شده صحیح نمی باشد" . "مجدد تلاش فرمایید";
-        }
+    } else {
+        return "دستور وارد شده صحیح نمی باشد" . "مجدد تلاش فرمایید";
     }
+}
 
-    $reply = [
-        'method' => 'sendMessage',
-        'chat_id' => $message['chat']['id'],
-        'text' => $replyText,
-    ];
-    echo json_encode($reply);
+$reply = [
+    'method' => 'sendMessage',
+    'chat_id' => $message['chat']['id'],
+    'text' => $replyText,
+];
+echo json_encode($reply);
 //gettype($userInput) == gettype($int)
